22688ae9e68d5cbf8c768a00253412049c42e8e3
QingYang1807 <fdgk2016@sina.com>
2019-03-09 20:51:47 +0800

    Create a new repository and import the files
    Create a new repository and import the files

---
+++ elmDemo/app/src/main/java/com/example/elmdemo/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.elmdemo;
+
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);// 全屏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onCreate() {
+        Log.e("20164206140-刘峰","onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("20164206140-刘峰","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("20164206140-刘峰","onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("20164206140-刘峰","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("20164206140-刘峰","onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e("20164206140-刘峰","onRestart");
+    }
+}
